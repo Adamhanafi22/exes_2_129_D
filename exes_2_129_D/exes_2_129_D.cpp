c73a65ca143014258723eb687e58b73dac0f0889
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-05-12 10:32:15 +0700

    Add project files.

---
+++ exes_2_129_D/exes_2_129_D.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void BinarySearch(string dam[16], int n, string name) {
+	int adam = 0, putra = n - 1;
+	int mid c = (adam + putra) / 2;
+
+	while (adam <= putra) {
+		if (dam[mid] == name) {
+			cout << "Nama" << name << " Di temukan pada indeks ke - " << mid << endl;
+			return;
+
+		}
+		else if (dam[mid] < name) {
+			adam = mid + 1;
+		}
+		else
+		{
+			adam = mid - 1;
+		}
+		mid = (adam + putra) / 2;
+	}
+	cout << "nama" << name << " Not found" << endl;
+	{
+
+	}
+}
+
+int main() {
+	const int MAX_LENGHT
+}
+
